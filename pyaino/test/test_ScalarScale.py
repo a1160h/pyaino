a41c4eec6529a48228c03487432e212d71e544c7
Aiichiro Inoue <129284205+a1160h@users.noreply.github.com>
2025-10-12 09:32:26 +0900

    Add files via upload
    Add test for ScalrScale class.

---
+++ pyaino/test/test_ScalarScale.py
@@ -0,0 +1,35 @@
+from pyaino.Config import *
+from pyaino import Neuron
+from pyaino import LossFunctions as lf
+
+
+x = np.arange(2*3*4).reshape(4, 2, 3).astype(Config.dtype)
+
+model = Neuron.ScalarScale(optimize='Adam')
+loss  = lf.MeanSquaredError()
+
+y = model.forward(x)
+print('入力\n', x)
+print('出力\n', y)
+
+print('敢えて、gammaを乱数で初期化')
+model.gamma = np.random.randn(*model.gamma.shape, dtype=Config.dtype)
+print('gamma\n', model.gamma)
+
+print('学習により、modelが元の値を出力するように、gammaが調整される')
+for i in range(10000):
+    y = model.forward(x)
+    l = loss.forward(y, x)
+    gy = loss.backward()
+    gx = model.backward(gy)
+    model.update()
+    #print(i, l)
+
+print('学習後のgammaとbeta')
+print('gamma\n', model.gamma)
+
+y = model.forward(x)
+print('学習後の出力')
+print(y)
+    
+assert np.allclose(x, y, rtol=1e-3, atol=1e-3)
