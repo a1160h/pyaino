364e1532797bc2b58cecb63195821f545964ecc4
Aiichiro Inoue <129284205+a1160h@users.noreply.github.com>
2026-08-08 11:09:16 +0900

    Add files via upload
    Adjustments made to align with the revised implementations of CutOutBatch and CutOutBatchIx in common_function.

--- pyaino/test/test_CutOutBatch.py
+++ pyaino/test/test_CutOutBatch.py
@@ -1,38 +1,37 @@
 from pyaino.Config import *
 from pyaino import common_function as cf
 
-
 def smoke_test(get_batch, name):
     print(f'\n--- {name} : shuffle={get_batch.shuffle} ---')
 
-    for i in range(4):
-        get_batch.info()
+    expected_offsets = ((0, 1, 2), (0, 1, 2), (0, 1), (0, 1, 2))
 
-        assert get_batch.epoch == i
-        assert get_batch.offset == i % get_batch.step
-        assert np.all(get_batch.start_ix % get_batch.step == get_batch.offset)
+    for epoch, offsets in enumerate(expected_offsets):
+        assert get_batch.epoch == epoch
+        assert get_batch.n_batch == len(offsets)
 
-        n_batch = get_batch.n_batch
-        for j in range(n_batch):
+        for offset in offsets:
             y = get_batch()
             assert y.shape == (2, 4)
             assert np.all(np.diff(y, axis=1) == 1)
+            assert np.all(y[:, 0] % get_batch.step == offset)
 
             if not get_batch.shuffle:
-                print(y)
+                print('epoch', epoch, 'offset', offset, '\n', y)
 
 
 data = np.arange(20)
-block_size = (4, 4, 3)
+block_size = (4, 4)
 batch_size = 2
+step = 3
 
 for shuffle in (False, True):
     set_seed(0)
-    get_batch = cf.CutOutBatch(data, block_size, batch_size, shuffle)
+    get_batch = cf.CutOutBatch(data, block_size, batch_size, step, shuffle)
     smoke_test(get_batch, 'CutOutBatch')
 
     set_seed(0)
-    get_batch = cf.CutOutBatchIx(len(data), block_size, batch_size, shuffle)
+    get_batch = cf.CutOutBatchIx(len(data), block_size, batch_size, step, shuffle)
     smoke_test(get_batch, 'CutOutBatchIx')
 
 print('\nCutOutBatch / CutOutBatchIx smoke test OK')
